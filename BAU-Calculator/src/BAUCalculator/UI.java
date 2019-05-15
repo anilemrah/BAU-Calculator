8b08847019b449c7609f7332e64afe76285ae17f
grsfatma <fatmagurses1@gmail.com>
2019-05-15 12:56:12 +0300

    simple factory

--- BAU-Calculator/src/BAUCalculator/UI.java
+++ BAU-Calculator/src/BAUCalculator/UI.java
@@ -3,19 +3,32 @@ package BAUCalculator;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import javax.swing.*;
 
 public class UI implements ActionListener {
 	private final JFrame frame;
 	private final JPanel panel;
+	private final JPanel panel2;
 	private final JTextArea text;
+	private final JTextArea text2;
+	private final JTextArea text3;
+	private final JLabel label1;
+	private final JLabel label2;
+	private final JLabel label3;
+	int index = -1;
+
 	private final JButton but[], buttonAdd, buttonMinus, buttonMultiply, buttonDivide, buttonEqual, buttonCancel,
 			buttonSquareRoot, buttonSquare, buttonOneDevidedBy, buttonCos, buttonSin, buttonTan, buttonxpowerofy,
-			buttonlog, buttonrate;
+			buttonlog, buttonrate, buttonShift, convertButton;
+	private final JComboBox combobox1;
+	private final JComboBox combobox2;
+	private final JComboBox combobox3;
 	private final Calculator calc;
 
 	private final String[] buttonValue = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
@@ -24,8 +37,15 @@ public class UI implements ActionListener {
 		frame = new JFrame("Calculator PH");
 		frame.setResizable(false);
 		panel = new JPanel(new FlowLayout());
+		panel2 = new JPanel(new FlowLayout());
 
 		text = new JTextArea(2, 25);
+		text2 = new JTextArea(2, 25);
+		text3 = new JTextArea(2, 25);
+		label1 = new JLabel("Please select the type of unit you want to convert \n");
+		label2 = new JLabel("From                                               \n");
+		label3 = new JLabel("To                                                 \n");
+
 		but = new JButton[10];
 		for (int i = 0; i < 10; i++) {
 			but[i] = new JButton(String.valueOf(i));
@@ -45,6 +65,17 @@ public class UI implements ActionListener {
 		buttonxpowerofy = new JButton("x^y");
 		buttonlog = new JButton("log10(x)");
 		buttonrate = new JButton("x%");
+		buttonShift = new JButton("SHIFT");
+		convertButton = new JButton("Convert");
+
+		combobox1 = new JComboBox();
+		combobox2 = new JComboBox();
+		combobox3 = new JComboBox();
+
+		combobox1.addItem("Angle");
+		combobox1.addItem("Time");
+		combobox1.addItem("Temperature");
+		combobox1.addItem("Length");
 
 		buttonCancel = new JButton("C");
 
@@ -56,7 +87,7 @@ public class UI implements ActionListener {
 		frame.setSize(330, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(panel);
-
+		panel2.setVisible(false);
 		panel.add(text);
 
 		for (int i = 1; i < 10; i++) {
@@ -79,9 +110,19 @@ public class UI implements ActionListener {
 		panel.add(buttonxpowerofy);
 		panel.add(buttonlog);
 		panel.add(buttonrate);
-
+		panel.add(buttonShift);
 		panel.add(buttonEqual);
 		panel.add(buttonCancel);
+
+		panel2.add(label1);
+		panel2.add(combobox1);
+		panel2.add(label2);
+		panel2.add(text2);
+		panel2.add(combobox2);
+		panel2.add(label3);
+		panel2.add(text3);
+		panel2.add(combobox3);
+		panel2.add(convertButton);
 
 		buttonAdd.addActionListener(this);
 		buttonMinus.addActionListener(this);
@@ -96,15 +137,28 @@ public class UI implements ActionListener {
 		buttonxpowerofy.addActionListener(this);
 		buttonlog.addActionListener(this);
 		buttonrate.addActionListener(this);
-
+		buttonShift.addActionListener(this);
 		buttonEqual.addActionListener(this);
 		buttonCancel.addActionListener(this);
+
+		combobox1.addActionListener(this);
+		combobox2.addActionListener(this);
+		combobox3.addActionListener(this);
+		convertButton.addActionListener(this);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		final Object source = e.getSource();
+		
+		int temp = index;
+		index = combobox1.getSelectedIndex();
 
+		if(index != temp) {
+			combobox2.removeAllItems();
+			combobox3.removeAllItems();
+		}
+		
 		for (int i = 0; i < 10; i++) {
 			if (source == but[i]) {
 				text.replaceSelection(buttonValue[i]);
@@ -169,6 +223,63 @@ public class UI implements ActionListener {
 			writer(calc.reset());
 		}
 
+		if (source == buttonShift) {
+			panel.setVisible(false);
+			panel2.setVisible(true);
+			frame.add(panel2);
+
+		}
+		if (source == convertButton) {
+
+			String type = combobox1.getSelectedItem().toString();
+			String from = combobox2.getSelectedItem().toString();
+			String to = combobox3.getSelectedItem().toString();
+
+			long data = Long.parseLong(text2.getText());
+			ConvertionFactory convertionFactory = new ConvertionFactory();
+			Convertion convert1 = convertionFactory.getConvertionType(type);
+			String resultText = convert1.convert(data, from, to);
+			text3.setText(resultText);
+
+		}
+		if (combobox2.getItemCount() == 0 && combobox3.getItemCount() == 0) {
+			switch (index) {
+			case 0:
+				combobox2.addItem("Degree");
+				combobox2.addItem("Radian");
+				combobox3.addItem("Degree");
+				combobox3.addItem("Radian");
+				break;
+			case 1:
+				combobox2.addItem("Hour");
+				combobox2.addItem("Minute");
+				combobox2.addItem("Second");
+				combobox2.addItem("Milisecond");
+				combobox3.addItem("Hour");
+				combobox3.addItem("Minute");
+				combobox3.addItem("Second");
+				combobox3.addItem("Milisecond");
+				break;
+			case 2:
+				combobox2.addItem("Celcius");
+				combobox2.addItem("Fahrenheit");
+				combobox2.addItem("Kelvin");
+				combobox3.addItem("Celcius");
+				combobox3.addItem("Fahrenheit");
+				combobox3.addItem("Kelvin");
+				break;
+			case 3:
+				combobox2.addItem("Kilometer");
+				combobox2.addItem("Meter");
+				combobox2.addItem("Feet");
+				combobox2.addItem("Mile");
+				combobox3.addItem("Kilometer");
+				combobox3.addItem("Meter");
+				combobox3.addItem("Feet");
+				combobox3.addItem("Mile");
+				break;
+			}
+		}
 		text.selectAll();
 	}
 
@@ -188,4 +299,5 @@ public class UI implements ActionListener {
 			text.setText(Double.toString(num));
 		}
 	}
+
 }
